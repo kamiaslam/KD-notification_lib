75ec1850725c36403e213a79b72fbd28efb8a5ab
Ali Adnan <iamaliadnan@gmail.com>
2019-04-26 00:57:52 +0500

    Add meeting event in notification lib

--- lib/enums.ex
+++ lib/enums.ex
@@ -1,4 +1,4 @@
 import EctoEnum
 
-defenum NotificationsLib.Enums.NotificationType, :notification_type, [:public_event, :private_event]
+defenum NotificationsLib.Enums.NotificationType, :notification_type, [:public_event, :private_event, :meeting_event]
 defenum NotificationsLib.Enums.UserType, :user_type, [:patient, :doctor, :admin]
